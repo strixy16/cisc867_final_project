baf887b2b5d3a1a2ecf02f56a21b4ea5656e3191
strixy16 <k.l.scott16@gmail.com>
2021-02-16 14:08:09 -0500

    Added variables for createCSV and updated variable descriptions

--- erasmus_tumors.m
+++ erasmus_tumors.m
@@ -2,14 +2,24 @@ function opt = erasmus_tumors
 %Description: Configuration file for preprocessMHA for Erasmus tumor
 %             image set
 %
-%OUTPUT: opt - struct containing variables defined here
+%OUTPUT: opt - struct containing variables defined and described below
 %
 %Environment: MATLAB R2020b
 %Notes: 
 %Author: Katy Scott
-%Created: 14 Jan 2020
+%Created: 14 Jan 2021
+%Updates:
+% Feb 16, 2021 - updated comments, added OutputCSV for createCSV
 
-    % Location of image files for Erasmus tumour set
+    % Location of image files for tumour image set, for use in preprocessMHA
     opt.ImageLoc = "/Users/katyscott/Documents/ICC/Data/cholangio/Erasmus/tumors/";
+    % Location of bin folder containing tumour image slice set, for use in
+    % preprocess MHA and createCSV
+    % NOTE: this has subfolders NaN and zero in it 
     opt.BinLoc = "/Users/katyscott/Documents/ICC/Data/cholangio/Erasmus/bin_tumors/";
+    
+    % Spreadsheet of labels, excel file, for use in createCSV.m
+    opt.Labels = "/Users/katyscott/Documents/ICC/Data/RFS_Scout.xlsx";
+    % File name + location to output in createCSV.m
+    opt.OutputCSV = "/Users/katyscott/Documents/ICC/Data/Labels/Erasmus_RSF.csv";
 end
--- msk_tumor.m
+++ msk_tumor.m
@@ -7,12 +7,20 @@ function opt = msk_tumor
 %Environment: MATLAB R2020b
 %Notes: 
 %Author: Katy Scott
-%Created: 14 Jan 2020
+%Created: 14 Jan 2021
+%Updates:
+% Feb 16, 2021 - updated comments, added OutputCSV for createCSV
 
-    % Location of image files for Erasmus tumour set
+
+    % Location of image files for tumour image set, for use in preprocessMHA
     opt.ImageLoc = "/Users/katyscott/Documents/ICC/Data/cholangio/MSK/tumor/";
+    % Location of bin folder containing tumour image slice set, for use in
+    % preprocess MHA and createCSV
+    % NOTE: this has subfolders NaN and zero in it 
     opt.BinLoc = "/Users/katyscott/Documents/ICC/Data/cholangio/MSK/bin_tumor/";
     
-    % Spreadsheet of labels, excel file
+    % Spreadsheet of labels, excel file, for use in createCSV.m
     opt.Labels = "/Users/katyscott/Documents/ICC/Data/RFS_Scout.xlsx";
+    % File name + location to output in createCSV.m
+    opt.OutputCSV = "/Users/katyscott/Documents/ICC/Data/Labels/MSK_RSF.csv";
 end
