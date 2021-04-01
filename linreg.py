e4cfba46164f17645820199c943cb89899fcf156
Katy Scott <k.l.scott16@gmail.com>
2021-04-01 14:41:28 -0400

    Removed InputFunction class

--- linreg.py
+++ linreg.py
@@ -3,7 +3,7 @@
 # Author: Katy Scott
 # Created: April 1, 2021
 
-from imp import reload
+from importlib import reload
 import math
 import matplotlib.pyplot as plt
 import numpy as np
@@ -30,3 +30,88 @@ from input_function import InputFunction, _make_riskset
 import cph_loss
 import cindex_metric
 
+# Constants for development
+FILESTOLOAD = 2888 # 2888 is all of them
+imdim_from_preprocessing = 256 # must match opt.ImageSize in image preprocessing configuration files
+imdim_for_network = 256
+random_seed = 16
+
+# Path to CSVs that connect patient id to slices and rfs label
+zero_info_path = "/Users/katyscott/Documents/ICC/Data/Labels/" + str(imdim_from_preprocessing) +"/RFS_all_tumors_zero.csv"
+nan_info_path = "/Users/katyscott/Documents/ICC/Data/Labels/" + str(imdim_from_preprocessing) +"/RFS_all_tumors_NaN.csv"
+
+zero_image_path = '/Users/katyscott/Documents/ICC/Data/Images/Tumors/' + str(imdim_from_preprocessing) + '/Zero/'
+nan_image_path = '/Users/katyscott/Documents/ICC/Data/Images/Tumors/' + str(imdim_from_preprocessing) + '/NaN/'
+
+# Reading in info for zero background images
+info = pd.read_csv(zero_info_path)
+image_fnames = np.asarray(info.iloc[:, 0])
+pat_num = np.asarray(info.iloc[:, 1])
+slice_num = np.asarray(info.iloc[:, 2])
+rfs_event = np.asarray(info.iloc[:, 3])
+rfs_time = np.asarray(info.iloc[:, 4])
+
+print(rfs_event.shape)
+print(rfs_time[1])
+
+# Only loading in 100 number of files for development
+images = np.empty((1,imdim_for_network,imdim_for_network))
+file_count = 0
+for image_file in tqdm(image_fnames):
+    if file_count >= FILESTOLOAD:
+        break
+    else:
+        file_count += 1
+    #     print("Loading: ", image_file)
+        # Load in file as an numpy array
+        img = np.fromfile(zero_image_path + image_file)
+        # Reshape image from 1D to 2D array - need to nothardcode this, square root?
+        img_2D = np.reshape(img, (imdim_from_preprocessing,imdim_from_preprocessing))
+        # Scale image to this dimension, smooth image with Gaussian filter, pads with the reflection of the vector
+        # mirrored on the first and last values of the vector along each axis.
+        img_final = resize(img_2D, (imdim_for_network, imdim_for_network), anti_aliasing=True, mode='reflect')
+        # Not sure this next line is working, want an array with all the images as their own array in it
+        img_final_3D = np.reshape(img_final, (1,) + img_final.shape)
+        images = np.append(images, img_final_3D, axis=0)
+
+images = np.delete(images, 0, axis=0)
+
+plt.imshow(images[8], cmap='Greys')
+
+# Training and testing split
+split = 0.9
+train_slice_indices, test_slice_indices = pat_train_test_split(pat_num[:FILESTOLOAD], rfs_event[:FILESTOLOAD], 0.9, random_seed)
+
+print("Train: ", np.array(train_slice_indices).shape)
+print("Test: ", np.array(test_slice_indices).shape)
+
+train_slices = images[train_slice_indices,:,:]#[:][:]
+train_slices = train_slices.squeeze() # Remove first dim of size 1
+
+train_time = rfs_time[train_slice_indices]
+train_event = rfs_event[train_slice_indices]
+print("Training set: ", train_slices.shape)
+print("Training time labels: ", train_time.shape)
+print("Training event labels: ", train_event.shape)
+
+test_slices = images[test_slice_indices,:,:]
+test_slices = test_slices.squeeze() # Remove first dim of size 1
+
+test_time = rfs_time[test_slice_indices]
+test_event = rfs_event[test_slice_indices]
+print("Testing set: ", test_slices.shape)
+print("Testing time labels: ", test_time.shape)
+print("Testing event labels: ", test_event.shape)
+
+train_1D = np.reshape(train_slices, (train_slices.shape[0], train_slices.shape[1]*train_slices.shape[2]))
+test_1D = np.reshape(test_slices, (test_slices.shape[0], test_slices.shape[1]*test_slices.shape[2]))
+# train_1D = train_1D[0:100,:]
+# linreg_labels = train_event[0:100]
+print(train_1D.shape)
+print(test_1D.shape)
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+regression_model = LinearRegression().fit(train_1D, train_event)
+regression_model.score(test_1D, test_event)
