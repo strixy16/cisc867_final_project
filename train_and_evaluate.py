d560f0b199eeb90002a48603370ba7150b9afaac
Katy Scott <k.l.scott16@gmail.com>
2021-04-01 11:49:53 -0400

    Finished copying over polsterl tutorial code

--- train_and_evaluate.py
+++ train_and_evaluate.py
@@ -46,5 +46,95 @@ class TrainAndEvaluateModel:
         y_event = tf.expand_dims(y_event, axis=1)
         with tf.GradientTape() as tape:
             logits = self.model(x, training=True)
-            
-            train_loss = self.loss_fn(y_true=)
+
+            train_loss = self.loss_fn(y_true=[y_event, y_riskset], y_pred=logits)
+
+        with tf.name_scope("gradients"):
+            grads = tape.gradient(train_loss, self.model.trainable_weights)
+            self.optimizer.apply_gradients(zip(grads, self.model.trainable_weights))
+        return train_loss, logits
+
+    def train_and_evaluate(self):
+        ckpt = tf.train.Checkpoint(
+            step=tf.Variable(0, dtype=tf.int64),
+            optimizer=self.optimizer,
+            model=self.model)
+        ckpt_manager = tf.train.CheckpointManager(
+            ckpt, str(self.model_dir), max_to_keep=2)
+
+        if ckpt_manager.latest_checkpoint:
+            ckpt.restore(ckpt_manager.latest_checkpoint)
+            print(f"Latest checkpoint restored from {ckpt_manager.latest_checkpoint}.")
+
+        train_summary_writer = summary.create_file_writer(
+            str(self.model_dir / "train"))
+        val_summary_writer = summary.create_file_writer(
+            str(self.model_dir / "valid"))
+
+        for epoch in range(self.num_epochs):
+            with train_summary_writer.as_default():
+                self.train_one_epoch(ckpt.step)
+
+            # Run a validation loop at the end of each epoch.
+            with val_summary_writer.as_default():
+                self.evaluate(ckpt.step)
+
+        save_path = ckpt_manager.save()
+        print(f"Saved checkpoint for step {ckpt.step.numpy()}: {save_path}")
+        
+    def train_one_epoch(self, step_counter):
+        for x, y in self.train_ds:
+            train_loss, logits = self.train_one_step(
+                x, y["label_event"], y["label_riskset"])
+
+            step = int(step_counter)
+            if step == 0:
+                # see https://stackoverflow.com/questions/58843269/display-graph-using-tensorflow-v2-0-in-tensorboard
+                func = self.train_one_step.get_concrete_function(
+                    x, y["label_event"], y["label_riskset"])
+                summary_ops_v2.graph(func.graph, step=0)
+
+            # Update training metric.
+            self.train_loss_metric.update_state(train_loss)
+
+            # Log every 200 batches.
+            if step % 200 == 0:
+                # Display metrics
+                mean_loss = self.train_loss_metric.result()
+                print(f"step {step}: mean loss = {mean_loss:.4f}")
+                # save summaries
+                summary.scalar("loss", mean_loss, step=step_counter)
+                # Reset training metrics
+                self.train_loss_metric.reset_states()
+
+            step_counter.assign_add(1)
+
+    @tf.function
+    def evaluate_one_step(self, x, y_event, y_riskset):
+        y_event = tf.expand_dims(y_event, axis=1)
+        val_logits = self.model(x, training=False)
+        val_loss = self.loss_fn(y_true=[y_event, y_riskset], y_pred=val_logits)
+        return val_loss, val_logits
+
+    def evaluate(self, step_counter):
+        self.val_cindex_metric.reset_states()
+        
+        for x_val, y_val in self.val_ds:
+            val_loss, val_logits = self.evaluate_one_step(
+                x_val, y_val["label_event"], y_val["label_riskset"])
+
+            # Update val metrics
+            self.val_loss_metric.update_state(val_loss)
+            self.val_cindex_metric.update_state(y_val, val_logits)
+
+        val_loss = self.val_loss_metric.result()
+        summary.scalar("loss",
+                       val_loss,
+                       step=step_counter)
+        self.val_loss_metric.reset_states()
+        
+        val_cindex = self.val_cindex_metric.result()
+        for key, value in val_cindex.items():
+            summary.scalar(key, value, step=step_counter)
+
+        print(f"Validation: loss = {val_loss:.4f}, cindex = {val_cindex['cindex']:.4f}")
