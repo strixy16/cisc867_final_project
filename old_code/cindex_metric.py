b841d625519b6a129603bf7853e1d5554bcc7011
Katy Scott <k.l.scott16@gmail.com>
2021-04-13 22:18:26 -0400

    Moved old code to its own folder

--- old_code/cindex_metric.py
+++ old_code/cindex_metric.py
@@ -10,53 +10,51 @@ import tensorflow as tf
 from sksurv.metrics import concordance_index_censored
 
 class CindexMetric:
-    """
-    Computes concordance index across one epoch
-    """
-    
-    def reset_states(self):
-        """
-        Clear the buffer of collected values
-        """
+    """Computes concordance index across one epoch."""
+
+    def reset_states(self) -> None:
+        """Clear the buffer of collected values."""
         self._data = {
-                        "label_time": [],
-                        "label_event": [],
-                        "prediction": []
-                     }
-        
-    def update_state(self, y_true, y_pred):
-        """
-        Collect observed time, event indicator and predictions for a batch
-        
+            "label_time": [],
+            "label_event": [],
+            "prediction": []
+        }
+
+    def update_state(self, y_true: Dict[str, tf.Tensor], y_pred: tf.Tensor) -> None:
+        """Collect observed time, event indicator and predictions for a batch.
+
         Args:
             y_true - dictionary containing: 
                     1) label_time = tensor containing observed time for one batch
                     2) label_event = tensor containing event indicator for one batch
             y_pred - tf.Tensor containing predicted risk scores for one batch
         """
-        
         self._data["label_time"].append(y_true["label_time"].numpy())
         self._data["label_event"].append(y_true["label_event"].numpy())
         self._data["prediction"].append(tf.squeeze(y_pred).numpy())
-        
-    def result(self):
-        """
-        Computes the concordance index across collected values
-        
-        Returns:
-            result_data - dictionary containing computed metrics {String, float}
+
+    def result(self) -> Dict[str, float]:
+        """Computes the concordance index across collected values.
+
+        Returns
+        ----------
+        metrics : dict
+            Computed metrics.
         """
         data = {}
         # Combine labels and predictions into one dictionary
         for k, v in self._data.items():
             data[k] = np.concatenate(v)
-            
-        # Using c-index function from sksurv
-        results = concordance_index_censored(data["label_event"] == 1, data["label_time"], data["prediction"])
-        
+
+        # Using c-index function sksurv
+        results = concordance_index_censored(
+            data["label_event"] == 1,
+            data["label_time"],
+            data["prediction"])
+
         result_data = {}
         names = ("cindex", "concordant", "discordant", "tied_risk")
         for k, v in zip(names, results):
             result_data[k] = v
-        
+
         return result_data
